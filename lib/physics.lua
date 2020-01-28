c603a19ad90493345d3f5c5d3bdee21eaa02e5ef
Elias Buisson <elias.buisson@gmail.com>
2020-01-28 22:35:32 +0100

    added physics base file and box collision

---
+++ lib/physics.lua
@@ -0,0 +1,26 @@
+function point_in_box(x1, y1, x2, y2, width2, height2)
+  local cond1 = x1 <= x2 + width2 and x1 >= x2
+  local cond2 = y1 <= y2 + height2 and y1 >= y2
+
+  return cond1 and cond2
+end
+
+function box2box_collide(x1, y1, width1, height1, x2, y2, width2, height2)
+  local cond1 = point_in_box(x1, y1, x2, y2, width2, height2)
+  local cond2 = point_in_box(x1+width1, y1, x2, y2, width2, height2)
+  local cond3 = point_in_box(x1, y1+height1, x2, y2, width2, height2)
+  local cond4 = point_in_box(x1+width1, y1+height1, x2, y2, width2, height2)
+
+  return cond1 or cond2 or cond3 or cond4
+end
+
+-- function box2box_collide(x1, y1, width1, height1, x2, y2, width2, height2)
+--   if (x1 + width1 < x2) or
+--      (x1 >= x2 + width2) or
+--      (y1 + height1) > (y2 + height2) or
+--      (y1 + height1 ) <= y2 then
+--      return false
+--   end
+--   return true
+-- end
+
