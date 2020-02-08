ce5f379c9c05e9968a01e74e2a7dda374dab3753
Elias Buisson <elias.buisson@gmail.com>
2020-02-08 11:02:07 +0100

    addes sprite files

---
+++ effects.lua
@@ -0,0 +1,41 @@
+
+MAXANGLE = 0.1
+
+shake = 1.5
+
+maxoffset = 20.0
+
+-- trauma level between [0, 1]
+-- camera shake is trauma2 or trauma3:
+-- trauma 0.30 0.60 0.90 are 3% 22% 73%
+
+
+function lerp(a, b, w)
+  -- w between 0.0 and 1.0
+  -- a and b floats 
+  -- interpolation between a and b linear
+  return (1.0 - w) * a + w * b
+end
+
+function dotGradient(ix, iy, x, y)
+  dx = x - ix*1.0
+  dy = y - iy*1.0
+
+  return (dx * gradient)
+
+end
+
+function get_random_float()
+  local num = math.random() * 2 - 1.0
+  return num
+end
+
+function shake_camera()
+  local angle = MAXANGLE * shake * get_random_float()
+  local offsetx = maxoffset * shake * get_random_float()
+  local offsety = maxoffset * shake * get_random_float()
+
+  print(angle, offsetx, offsety)
+  return angle, offsetx, offsety
+end
+
